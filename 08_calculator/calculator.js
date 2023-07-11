9713da34c6a55647ba15e04f4653405e07a38871
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-07-11 16:43:37 -0400

    completed assignments 8-12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (i, j) {
+  return i + j;
 };
 
-const subtract = function() {
-	
+const subtract = function (i, j) {
+  return i - j;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function ([...args]) {
+  let product = 1;
+  for (let i = 0; i < args.length; i++) {
+    product *= args[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (i, j) {
+  return Math.pow(i, j);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
 
+  let firstNum = 1;
+  let secondNum = 0;
+
+  for (let i = 2; i <= count; i++) {
+    let current = firstNum + secondNum;
+    secondNum = firstNum;
+    firstNum = current;
+  }
+
+  return firstNum;
 };
+// let fn = 1,
+//   sn = 0,
+//   current = fn + sn;
+
+// function fib(num) {
+//   while (num >= 0) {
+//     current = fn;
+//     console.log(current);
+//     fn += sn;
+//     sn = current;
+//     num--;
+//   }
+// }
+// fib(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
