bc9085b745f4fcec8d501484aeea213c594e8816
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-07-11 12:05:53 -0400

    completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (numFloor, numCeil) {
+  if (!Number.isInteger(numFloor) || !Number.isInteger(numCeil)) return "ERROR";
+  if (numFloor < 0 || numCeil < 0) return "ERROR";
+  if (numFloor > numCeil) {
+    const temp = numFloor;
+    numFloor = numCeil;
+    numCeil = temp;
+  }
+  let sum = 0;
+  for (let i = numFloor; i < numCeil + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
